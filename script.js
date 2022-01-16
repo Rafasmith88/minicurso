727d7bc1d48b366ca3f8e21cf8f18a76d6f28ebe
Rafael Smith <smithyoungflu@gmail.com>
2022-01-16 09:08:04 -0300

    finalizr curso

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
-const especie = document.querySelector('#Especie');
-const status = document.querySelector('#Status');
+const especie = document.querySelector('#especie');
+const condicao = document.querySelector('#status');
+
+traduzirCondicao = (data) => {
+    if (data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status =='Alive'){
+        return 'Sim';
+    }else {
+        return 'Não Está morto';
+    }
+}
+gerarValorAleatorio = () => {
+    return Math.floor(Math.random() *671);
+}
+
+pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}` ,) {
+        method:'GET',
+        headers: {
+            Acept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt =data.name;
+        nomeDoPersonagem.innerHTML =data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = traduziCondicao(data);
+
+    });
+}
+
+botao.onclick = pegarPersonagem;
